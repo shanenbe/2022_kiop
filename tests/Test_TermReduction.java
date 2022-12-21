1449aa617197921eb7537cad9c8b973dda692d76
Marvin Händel <marvin.haendel@stud.uni-due.de>
2022-12-21 14:07:37 +0100

    Added new Tests and Fixed old Ones

--- tests/Test_TermReduction.java
+++ tests/Test_TermReduction.java
@@ -2,22 +2,45 @@ package tests;
 
 import junit.framework.TestCase;
 import lambda.Environment;
+import lambda.Memory;
 import lambda.lterm.*;
 import lambda.lterm.Record;
 import lambda.lterm.literal.*;
+import lambda.lterm.ref.Address;
+import lambda.lterm.ref.Assign;
 import lambda.lterm.sumtype.Case;
 import lambda.lterm.sumtype.InL;
 import lambda.type.*;
 
 public class Test_TermReduction extends TestCase {
 
+    public void test_memory()
+    {
+        Memory mem = new Memory();
+        True t = this.True();
+        Address adress=mem.create_ref_in_memory(t);
+        False f=this.False();
+        Assign assign = new Assign(adress,f);
+        assign.reduce(mem);
+        assertEquals(f,mem.dereference(adress));
+        LTerm num = this.Num(5);
+        assign=new Assign(adress,num);
+        try {
+            assign.reduce(mem);
+        }
+        catch (RuntimeException E)
+        {
+            assertNotNull(E);
+        }
 
+    }
     /**
      *
      * (Lp:{x:Num, y:Num}. p.x) {x=42, y=666} -> 42
      * (Lp:{x:Num, y:Num}. p.x) {x=42, y=666} : Num
      *
      */
+
     public void test_record_01() {
         Record r = Record("x", 42, "y", 666);
         Abstraction abs =
@@ -25,7 +48,7 @@ public class Test_TermReduction extends TestCase {
                         Proj(Var("p"), "x")
                 );
         Application app = App(abs, r);
-        LTerm result = app.reduceAll();
+        LTerm result = app.reduceAll(this.Mem());
         assertEquals(result, Num(42));
 
     }
@@ -48,11 +71,12 @@ public class Test_TermReduction extends TestCase {
 
 
     public void test_iif() {
+        Memory mem = Mem();
         Iif iif = Iif(True(), True(), Num(42));
         assertEquals(SumType(Bool(), Num()), iif.type_of(E()));
-        assertEquals(Inl(SumType(Bool(), Num()), True()), iif.reduce());
+        assertEquals(Inl(SumType(Bool(), Num()), True()), iif.reduce(mem));
         Case _case = Case(iif, SumType(Bool(), Num()), "a", True(), "b", True() );
-        assertEquals(True(), _case.reduce().reduce());
+        assertEquals(True(), _case.reduce(mem).reduce(mem));
 
 
 
@@ -69,18 +93,19 @@ public class Test_TermReduction extends TestCase {
      * if(true) then 23 else true: Fehler
      */
     public void test_if() {
+        Memory mem=this.Mem();
         If IF = If(True(), Num(23), Num(42));
         assertEquals(Num(), IF.type_of(E()));
-        assertEquals(Num(23), IF.reduce());
+        assertEquals(Num(23), IF.reduce(mem));
 
         IF = If(False(), Num(23), Num(42));
         assertEquals(Num(), IF.type_of(E()));
-        assertEquals(Num(42), IF.reduce());
+        assertEquals(Num(42), IF.reduce(mem));
 
         IF = If(Not(True()), Num(23), Num(42));
         assertEquals(Num(), IF.type_of(E()));
-        assertEquals(If(False(), Num(23), Num(42)), IF.reduce());
-        assertEquals(Num(42), IF.reduce().reduce());
+        assertEquals(If(False(), Num(23), Num(42)), IF.reduce(mem));
+        assertEquals(Num(42), IF.reduce(mem).reduce(mem));
     }
 
 
@@ -97,7 +122,7 @@ public class Test_TermReduction extends TestCase {
     // not (not false) -> false
     public void test_04_not_not_false() {
         LTerm application = App(Not(),App(Not(), False()));
-        LTerm result = application.reduceAll();
+        LTerm result = application.reduceAll(this.Mem());
         assertEquals(result, False());
     }
 
@@ -107,7 +132,7 @@ public class Test_TermReduction extends TestCase {
         False t = False();
         LTerm application = App(n, t);
 
-        LTerm result = application.reduce();
+        LTerm result = application.reduce(this.Mem());
 
         assertEquals(result, True());
     }
@@ -118,7 +143,7 @@ public class Test_TermReduction extends TestCase {
         True t = True();
         LTerm application = App(n, t);
 
-        LTerm result = application.reduce();
+        LTerm result = application.reduce(this.Mem());
 
         assertEquals(result, False());
     }
@@ -137,6 +162,7 @@ public class Test_TermReduction extends TestCase {
 //
 //    }
 
+    private Memory Mem(){return new Memory();}
     private Application App(LTerm abstraction, LTerm variable) {
         return new Application(abstraction, variable);
     }
